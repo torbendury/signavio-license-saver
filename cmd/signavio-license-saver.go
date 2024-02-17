d415679670022d27a4d6cb6447ac66ea2d53f07b
Torben Dury <torbendury@googlemail.com>
2024-02-18 00:45:20 +0100

    refactor: main vars and flag handling extraction
    Signed-off-by: Torben Dury <torbendury@googlemail.com>

--- cmd/signavio-license-saver.go
+++ cmd/signavio-license-saver.go
@@ -11,31 +11,19 @@ import (
 	"github.com/torbendury/signavio-license-saver/pkg/signavio"
 )
 
-var signavioTenant string
-var signavioUser string
-var signavioPassword string
-var signavioURL string
-var allowlist string
+var (
+	signavioTenant   string
+	signavioUser     string
+	signavioPassword string
+	signavioURL      string
+	allowlist        string
+	logger           *slog.Logger
+)
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	flag.StringVar(&signavioTenant, "tenant", "", "Signavio tenant ID")
-	flag.StringVar(&signavioUser, "user", "", "Signavio user")
-	flag.StringVar(&signavioPassword, "password", "", "Signavio password")
-	flag.StringVar(&signavioURL, "url", "", "Signavio URL (e.g. https://editor.signavio.com, no trailing slash)")
-	flag.StringVar(&allowlist, "allowlist", "", "Optional: Comma separated list of emails to keep")
-	flag.Parse()
-
-	if signavioTenant == "" || signavioUser == "" || signavioPassword == "" || signavioURL == "" {
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-
-	if allowlist == "" {
-		logger.Info("No allowlist provided, at least keeping API user")
-		allowlist = signavioUser
-	}
+	ensureFlags()
 
 	allowedUsers := strings.Split(allowlist, ",")
 	if !slices.Contains(allowedUsers, signavioUser) {
@@ -44,8 +32,7 @@ func main() {
 	}
 
 	client := signavio.New(signavioTenant, signavioURL, signavioUser, signavioPassword, logger)
-	err := client.Login()
-	if err != nil {
+	if err := client.Login(); err != nil {
 		logger.Error("Login failed", "error", err)
 		os.Exit(1)
 	}
@@ -79,12 +66,33 @@ func main() {
 				if err != nil {
 					logger.Error("GetJobStatus failed", "error", err)
 				}
+				// NOTE: This lets the whole goroutine sleep, which is not ideal.
+				// In this case, it is not a problem because Signavio allows only one job at a time,
+				// so it would not be possible to run another job in parallel anyway
 				time.Sleep(500 * time.Millisecond)
 			}
 			logger.Info("Job finished", "job", *job)
-
 		} else {
 			logger.Info("Keeping user", "email", user.Rep.Email)
 		}
+	}
+}
+
+func ensureFlags() {
+	flag.StringVar(&signavioTenant, "tenant", "", "Signavio tenant ID")
+	flag.StringVar(&signavioUser, "user", "", "Signavio user")
+	flag.StringVar(&signavioPassword, "password", "", "Signavio password")
+	flag.StringVar(&signavioURL, "url", "", "Signavio URL (e.g. https://editor.signavio.com, no trailing slash)")
+	flag.StringVar(&allowlist, "allowlist", "", "Optional: Comma separated list of emails to keep")
+	flag.Parse()
+
+	if signavioTenant == "" || signavioUser == "" || signavioPassword == "" || signavioURL == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if allowlist == "" {
+		logger.Info("No allowlist provided, at least keeping API user")
+		allowlist = signavioUser
 	}
 }
